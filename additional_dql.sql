d86d70e39967444a772ac86e4a25cd55411c5f89
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2026-01-08 08:51:04 -0300

    add: More DQL's

--- additional_dql.sql
+++ additional_dql.sql
@@ -103,3 +103,24 @@ WHERE r.reservation_date >= (CURRENT_DATE - INTERVAL '1 month')
 SELECT *
 FROM books
 WHERE title NOT LIKE '% %';
+
+SELECT b.title,
+       COUNT(*) AS total_loans
+FROM loans l
+JOIN book_copies bc USING(copy_id)
+JOIN books b        USING(book_id)
+GROUP BY b.title
+ORDER BY total_loans DESC;
+
+SELECT MIN(loan_date) AS first_loan,
+       MAX(loan_date) AS last_loan
+FROM loans;
+
+SELECT AVG((due_date - loan_date)) AS avg_loan_duration_days
+FROM loans
+WHERE return_date IS NOT NULL;
+
+SELECT SUM(
+    CASE WHEN status = 'active' THEN 1 ELSE 0 END
+) AS total_active_reservations
+FROM reservations;
