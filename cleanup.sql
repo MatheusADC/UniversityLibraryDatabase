753d581f84f2925d343e6597956f8fd693c8af02
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2026-01-24 15:31:03 -0300

    fix: Aliases

--- cleanup.sql
+++ cleanup.sql
@@ -3,7 +3,7 @@ FROM pg_stat_user_tables
 ORDER BY n_dead_tup DESC;
 
 VACUUM VERBOSE books;
-VACUUM VERBOSE eventos;
+VACUUM VERBOSE events;
 
-SELECT date_trunc('month', now()) AS referencia,
-       pg_total_relation_size('loans')/1024/1024 AS tamanho_mb;
+SELECT date_trunc('month', now()) AS reference,
+       pg_total_relation_size('loans')/1024/1024 AS size_mb;
